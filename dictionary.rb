7293ff0ae855aeeb9b3cf589722f40bd4eb58c60
Paul Scarrone <paul.scarrone@gmail.com>
2012-05-31 23:48:58 -0400

    Updated so Dict would should the last element for definition which is the last element in all indexes
    Indexes do not have uniform length

--- dictionary.rb
+++ dictionary.rb
@@ -1,17 +1,19 @@
 class WordListProcessor
   def self.LoadDictionary()
-    wordCollection = Array.new
+    dictHash = Hash.new
     Dir.foreach('DataFiles/dictionary_source/') do |f|
       if f.match('index')
         puts f
+        wordCollection = Array.new
         IO.foreach('DataFiles/dictionary_source/'+f) do |line|
           if(line[0] + line[1] != '  ')
             wordCollection.push(WordListObject.new(line))
           end
         end
+        dictHash[f] = wordCollection
       end
     end
-    return wordCollection
+    return dictHash
   end
 end
 
@@ -26,8 +28,8 @@ class WordListObject
     row = dictString.split
     @word = row[0]
     @type = row[1]
-    @definition = row[8]
+    @definition = row.last
   end
 end
 
-something = WordListProcessor.LoadDictionary
+#something = WordListProcessor.LoadDictionary
