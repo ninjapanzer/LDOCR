2f1a703913f0dea0b72d2a6fe5ea80d331e59634
Paul Scarrone <pscarrone@thinkthroughmath.com>
2015-07-06 00:04:13 -0400

    delete unneeded files

--- irb_helper.rb
+++
@@ -1,7 +0,0 @@
-require_relative 'config/active_record_config'
-require_relative 'models'
-
-ActiveRecord::Base.establish_connection(
-  :adapter  => "sqlite3",
-  :database => "db/LDOCR.sqlite3"
-)
