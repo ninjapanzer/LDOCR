bb7e81f0be76ac0f76d9b816a2f20e790c5f2284
Paul Scarrone <pscarrone@thinkthroughmath.com>
2015-07-05 23:47:30 -0400

    simplify dictionary loader and add specs for the dictionary parsing process

---
+++ dictionary_processor.rb
@@ -0,0 +1,33 @@
+require_relative 'models'
+
+class DictionaryProcessor
+  attr_reader :datafiles
+
+  def initialize(config = {})
+    @datafiles = config.fetch(:datafiles, 'Datafiles/dictionary_source/')
+  end
+
+  def load_dictionary
+    dict = {}
+    index_files = Dir.foreach(datafiles).select { |f| f.match('index') }
+    index_files.each { |f| load_dictionary_index(f) }
+  end
+
+  def load_dictionary_index(file)
+    file_source = "#{datafiles}#{file}"
+    IO.foreach(file_source) do |line|
+      if(line[0] + line[1] != '  ')
+        Word.create(extract(line))
+      end
+    end
+  end
+
+  def extract(dictString)
+    row = dictString.split
+    {
+      word: row[0],
+      part_of_speech: row[1],
+      definition: row.last
+    }
+  end
+end
---
+++ spec/dictionary_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+require_relative '../dictionary_processor'
+
+describe 'DictionarySpec' do
+  let(:processor) { DictionaryProcessor.new datafiles: 'spec/fixtures/word_sources/' }
+
+  before :each do
+    processor.load_dictionary
+  end
+
+  it 'loads all index files' do
+    expect(Word.count).to eq(10)
+  end
+
+  it 'loads five verbs and five nouns' do
+    expect(Word.where(part_of_speech: 'v').count).to eq(5)
+    expect(Word.where(part_of_speech: 'n').count).to eq(5)
+  end
+
+  it 'can find a word by name' do
+    specific_word = Word.where(part_of_speech: 'v', word: 'abacinate').first.definition
+    expect(specific_word).to eq('02168378')
+  end
+end
